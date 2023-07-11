d0ef9e3eb21d39a35cdf137a4761115f977d9e4c
strikerSigma <ibrahimsomeonenotimportant@gmail.com>
2023-07-11 16:22:29 +0500

    Ui finished Only functionality remaining

--- src/App.jsx
+++ src/App.jsx
@@ -68,13 +68,59 @@ function App() {
         <p className='Sname'>$18</p>
       </div>
       </div>
-      <div className='form'>
+      <div className='form md:flex mt-20'>
         <form>
           <p className='title4'>Contact Us</p>
           <input type='text' placeholder='Username' className='input'/>
           <input type='text' placeholder='Email' className='input'/>
-          <textarea type='text' placeholder='Message' className='input'/>
+          <textarea type='text' placeholder='Message' className='input text'/>
+          <button type='submit' className='button ml-20 px-20 bg-white'>Send</button>
         </form>
+        <div className='text-white text-lg p-14 md:pt-32'>
+          <span className='text-2xl px-5'>Or</span>
+          <span>Connect via cell-phone: </span><p className='px-16'>
+            0301-12345678</p>
+          </div>
+        <div className='px-10 maps'><img src='./src/assets/Maps.png'/></div>
+      </div>
+      <div className='cards md:flex'>
+        <div className='m-20'><img src='./src/assets/Card1.png'/></div>
+        <div className='m-20'><img src='./src/assets/Card2.png'/></div>
+      </div>
+    </div>
+    <div className='footer md:flex bg-black'>
+      <div className='p-20 pl-10'><p className='title4 title5'>Follow</p>
+      <p className='text-white pl-16'>You can Enter your Email to recieve
+         the hotest deals and membership alerts right at your inbox.</p>
+         <div className='relative w-40'>
+          <input type='email' className='input Email ' placeholder='Email'/>
+          <button className='md:absolute button button2'>Submit</button></div>
+         </div>
+      <div className='p-10 '><p className='title4 title5'> Hours</p>
+      <div>
+        <span className='text-white pl-16'>Monday</span>
+        <span className='text-white pl-32'>9:00-18:00</span>        
+      </div>
+      <div>
+        <span className='text-white pl-16'>Tuesday</span>
+        <span className='text-white pl-28 ml-3'>9:00-18:00</span>        
+      </div>
+      <div>
+        <span className='text-white pl-16'>Wednesday</span>
+        <span className='text-white pl-24 ml-1.5'>9:00-18:00</span>        
+      </div>
+      <div>
+        <span className='text-white pl-16'>Thrusday</span>
+        <span className='text-white pl-28 ml-2'>9:00-18:00</span>        
+      </div>
+      <div>
+        <span className='text-white pl-16'>Friday</span>
+        <span className='text-white pl-36'>9:00-18:00</span>        
+      </div>
+      <div>
+        <span className='text-white pl-16 mt-2'>Other Days</span>
+        <span className='text-white pl-36'>Closed</span>        
+      </div>
       </div>
     </div>
     </div>
